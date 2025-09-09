b94f03759a3ecfbfb89c30c2626959016abfefb8
MinalChhatre10 <minalchhatre10@gmail.com>
2025-09-09 13:03:31 +0530

    load record3

--- routes/records.js
+++ routes/records.js
@@ -249,19 +249,21 @@ router.get("/my", auth, async (req, res) => {
     if (!pid)
       return res.status(404).json({ error: "Patient record not found" });
 
-    const r = await sql.query`
+      const r = await sql.query`
   SELECT 
     mr.record_id,
     mr.file_path,
     mr.uploaded_at,
     mr.description,
-    u.full_name AS doctor_name
+    u.full_name AS doctor_name,
+    d.specialization
   FROM MedicalRecords mr
   LEFT JOIN Doctors d ON mr.doctor_id = d.doctor_id
   LEFT JOIN Users u ON d.user_id = u.user_id
   WHERE mr.patient_id = ${pid}
   ORDER BY mr.uploaded_at DESC
 `;
+
 
 
     const containerClient = await getContainerClient();
